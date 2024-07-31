616d6ce8f44d5858f4a6a9430d448659ce80827c
samirvithlani <samir.vithlani83955@gmail.com>
2024-07-31 13:52:54 +0530

    initial

---
+++ basics/basci1.js
@@ -0,0 +1,29 @@
+console.log("Hello")
+console.warn("This is a warning")
+console.error("This is an error")
+
+//number
+var a = 10
+console.log(a)
+console.log("value of a = ",a)
+console.log("type of a = ",typeof a)
+
+var per = 89.90 //float
+console.log("per = ",per)
+console.log("type of per = ",typeof per)
+
+//string    
+
+var user = "ram"
+console.log("user = ",user)
+console.log("type of user = ",typeof user)
+
+//boolean
+var isMarried = true
+console.log("isMarried = ",isMarried)
+console.log("type of isMarried = ",typeof isMarried)
+
+//undefined
+var x
+console.log("x = ",x)
+console.log("type of x = ",typeof x)
