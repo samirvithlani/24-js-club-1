ac16df7d60aea04b34df8b6110bc43f417b15279
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-06 19:22:53 +0530

    if else

---
+++ ifelse/ifdemo1.js
@@ -0,0 +1,47 @@
+// var x = 0
+// if(x>0){
+//     console.log("x is positive")
+// }
+// else{
+//     console.log("x is negative")
+// }
+
+// == compare only value
+// var x = 19
+
+// if(x%2 ==0){
+//     console.log("Even")
+// }
+// else{
+//     console.log("Odd")
+// }
+
+//== , ===
+//== check only value
+//=== check value and datatype
+
+// var x =10 //10 // number
+// var y ="10" //10 // string
+
+
+// if(x===y){
+//     console.log("Equal")
+// }
+// else{
+//     console.log("Not Equal")
+// }
+
+
+
+var x =100
+
+if(x>=100){
+
+    var p =20;
+}
+else{
+    let p =30;
+}
+
+console.log(p) // p is not defined
+
---
+++ operators/AsignmentOp1.js
@@ -0,0 +1,41 @@
+// var no1 = 10
+// var no2 = 20
+
+// var temp;
+
+// temp = no1 // temp = 10
+// no1 = no2 // no1 = 20
+// no2 = temp // no2 = 10
+
+// console.log("no1 ",no1) // 20
+// console.log("no2 ",no2) // 10
+
+
+var no1 = 10
+var no2 = 20
+
+no1 = no1 + no2  // no1 = 10 + 20 = 30
+no2 = no1 - no2 // 30 - 20 = 10
+no1 = no1 - no2 // 30 - 10 = 20
+
+console.log("no1 ",no1) // 30
+console.log("no2 ",no2) // 10
+
+
+
+
+var x =10
+//x = x  + 1 
+x+=1 //11
+x-=1 //10
+x*=1 //10
+x/=1 //10
+console.log("x = ",x) 
+
+
+var p =10
+var ans = p ** 4
+console.log("ans = ",ans) // 100
+
+
+console.log(p*p*p)
