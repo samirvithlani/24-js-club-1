b063ce774102e614ac0f39a91386d08185bdef09
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-20 18:54:00 +0530

    string

---
+++ string/string1.js
@@ -0,0 +1,49 @@
+var user = "ramayan"
+console.log(user)
+// console.log(user[0])
+// console.log(user[1])
+// console.log(user[2])
+
+// console.log(user[3])
+
+//to find len of string...
+
+
+
+
+
+var len = user.length; //it is property of string
+console.log("len of user = ",len)
+
+for(let i=0;i<user.length;i++){
+    console.log(user[i])
+}
+
+
+
+//string immutable...4
+
+var fname = "ram"
+var lname = "kumar"
+
+var fullname = fname +" "+ lname
+//fullname + "ravi" // immutable
+fullname = fullname + "ravi" // immutable
+//fullname = "ravi"
+console.log(fullname)
+
+//hello this is ahmedabad 
+
+//no space find...
+
+
+
+
+
+
+
+
+
+
+
+
