314238f0ae7303863e36adfcd4c76ef54c77ddf6
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-27 19:16:51 +0530

    include

--- string/string8.js
+++ string/string8.js
@@ -1,6 +1,16 @@
 var email = "sam@gmail.com"
-var flag = email.includes("@")
-console.log(flag)
+// var flag = email.includes("@")
+// console.log(flag)
 
 //includes --> 'x' --> true
 
+var flag = false;
+for(let i=0;i<email.length;i++){
+    //console.log(email[i])
+    if(email[i] == "x"){
+        flag = true;
+        break;
+    }
+}
+
+console.log(flag)
