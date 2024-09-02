98370cbfa16e0830cdb311046c39efd8bf536d79
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-02 19:17:37 +0530

    prompt

---
+++ promt/promtdemo.js
@@ -0,0 +1,12 @@
+//var no = prompt("enter no :")
+var  no = parseInt(prompt("enter no :"))
+//printf
+//"12"+100
+console.log("type of no = ",typeof no)
+console.log("no = ",no + 100)
+
+var name = prompt("enter name :")
+console.log("name = ",name)
+
+//palindrome....
+//naman  --> naman
