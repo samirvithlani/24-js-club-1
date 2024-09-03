a1cd59e186d386e3bf8a0c8420ae2d8541d02b45
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-03 19:30:55 +0530

    functions

---
+++ functions/function1.js
@@ -0,0 +1,49 @@
+//defination // {} --> body..
+
+function test(){
+    console.log("test function called....")
+}
+
+//without return type and without argument
+function drawLine(){
+    //console.log("_____________________________________________________________")
+    console.log("-------------------------------------------------------------")
+    //
+}
+
+//calling part..
+
+test(); // test function called....
+
+
+//10
+drawLine();
+//20 50
+drawLine();
+//100//
+drawLine();
+
+
+function add(){
+
+    let a = 100
+    let b = 200
+
+    let ans = a + b;
+    console.log("Addition is : "+ans)
+}
+
+function sub(){
+    
+        let a = 100
+        let b = 200
+    
+        let ans = a - b;
+        console.log("Substraction is : "+ans)
+}
+add()
+drawLine()
+sub()
+
+
+
