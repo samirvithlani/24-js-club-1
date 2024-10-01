ad11b838985652eac951a1d2d78f351a3e894542
samirvithlani <samir.vithlani83955@gmail.com>
2024-10-01 19:21:13 +0530

    data

---
+++ array/jsonarray/jsonarray5.js
@@ -0,0 +1,28 @@
+var cases = [
+    {
+        partyName:"Samir",
+        partyAge: 22,
+        violation:"over speed",
+        fine: 2000,
+        location: "Ahmedabad",
+        ipcs:["IPC 279","IPC 337"]
+    },
+    {
+        partyName:"Raj",
+        partyAge: 25,
+        violation:"drunk and drive",
+        fine: 5000,
+        location: "Surat",
+        ipcs:["IPC 185","IPC 337"]
+
+    },
+    {
+        partyName:"Ram",
+        partyAge: 30,
+        violation:"over speed",
+        fine: 2000,
+        location: "Vadodara",
+        ipcs:["IPC 279","IPC 337"]
+    }
+]
+//partName -->violation --> ipcs
