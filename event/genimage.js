6dfa332aeffa4863f0da109b605c91e654a99845
samirvithlani <samir.vithlani83955@gmail.com>
2024-10-21 19:17:09 +0530

    onmouse leave

---
+++ event/genimage.js
@@ -0,0 +1,5 @@
+const postImage = () => {
+  const image = document.getElementById("image"); //<img>
+  image.src =
+    "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+};
