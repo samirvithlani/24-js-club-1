7f237c2a9b1de1dfdccb222cbee2f97fb6c9e901
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-16 19:19:16 +0530

    onblur

--- event/onblur.js
+++ event/onblur.js
@@ -3,10 +3,40 @@ const emailList = ["raj@gmail.com","parth@gmail.com","amit@gmail.com","abc@gmail
 
 const getEmail =()=>{
 
-    //console.log("blur event called...")
 
-    //abc@gmail.com.. 
-    //message email has been taken...
-    //x@gmail.com
-    //email is available...
+    const email = document.getElementById("email")
+    const message = document.getElementById("message")
+
+    var flag  = false;
+
+    // for(let i=0;i<emailList.length;i++){
+
+    //     if(email.value == emailList[i]){
+    //         flag = true
+    //     }
+    // }
+    // if(flag==false){
+    //     message.innerHTML = "email is avaialble..."
+    // }
+    // else{
+    //     message.innerHTML = "emaill taken"
+    // }
+
+    // var x = emailList.find((e)=>e == email.value)
+    // //console.log(x)
+    // if(x==undefined){
+    //     message.innerHTML = "email available"
+    // }
+    // else{
+    //      message.innerHTML = "email taken*"
+    // }
+
+
+    // if(emailList.includes(email.value)){
+    //         message.innerHTML = "email taken*"
+    // }
+    // else{
+    //     message.innerHTML = "email avaialble.."
+    // }
+    
 }
