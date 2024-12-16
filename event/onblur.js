289e821e4ed2e44de0121b11cab3975c89ed06e9
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-16 18:50:23 +0530

    emailList

---
+++ event/onblur.js
@@ -0,0 +1,12 @@
+
+const emailList = ["raj@gmail.com","parth@gmail.com","amit@gmail.com","abc@gmail.com"]
+
+const getEmail =()=>{
+
+    //console.log("blur event called...")
+
+    //abc@gmail.com.. 
+    //message email has been taken...
+    //x@gmail.com
+    //email is available...
+}
--- formb/form2.js
+++ formb/form2.js
@@ -1,44 +1,35 @@
-const submitHandler = (event)=>{
-    event.preventDefault();
-    const name = document.getElementById("name") //<input>
-    const nameError = document.getElementById("nameError")
-    const genderError = document.getElementById("genderError")
-    //console.log(name.value)
-    if(name.value.trim().length<=0){
-        
-        //console.log("name is required*")
-        nameError.innerHTML = "Name is Required*"
-        nameError.style.color="red"
-    }
-    else{
-       // console.log(name.value)
-       const nameError = document.getElementById("nameError")
-       nameError.innerHTML = name.value
-       nameError.style.color="black"
-    }
+const submitHandler = (event) => {
+  event.preventDefault();
+  const name = document.getElementById("name"); //<input>
+  const nameError = document.getElementById("nameError");
+  const genderError = document.getElementById("genderError");
+  //console.log(name.value)
+  if (name.value.trim().length <= 0) {
+    //console.log("name is required*")
+    nameError.innerHTML = "Name is Required*";
+    nameError.style.color = "red";
+  } else {
+    // console.log(name.value)
+    const nameError = document.getElementById("nameError");
+    nameError.innerHTML = name.value;
+    nameError.style.color = "black";
+  }
 
-    var isChecked =false;
-    var genderValue =""
-    const gender = document.getElementsByName("gender") //[in,in]
-    for(let i=0;i<gender.length;i++){
-        //in[0] -->
-        //in[1] -->
-        if(gender[i].checked){
-            genderValue = gender[i].value
-            isChecked = true;
-        }
-        
+  var isChecked = false;
+  var genderValue = "";
+  const gender = document.getElementsByName("gender"); //[in,in]
+  for (let i = 0; i < gender.length; i++) {
+    //in[0] -->
+    //in[1] -->
+    if (gender[i].checked) {
+      genderValue = gender[i].value;
+      isChecked = true;
     }
+  }
 
-    if(isChecked == false){
-        genderError.innerHTML = "select gender *"
-    }
-    else{
-        genderError.innerHTML =genderValue
-        
-    }
-   
-
-
-
-}
+  if (isChecked == false) {
+    genderError.innerHTML = "select gender *";
+  } else {
+    genderError.innerHTML = genderValue;
+  }
+};
