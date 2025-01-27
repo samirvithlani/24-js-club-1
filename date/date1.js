bba6f3d0ed076599b5edfe8bdb9b4cfeaa033de5
samirvithlani <samir.vithlani83955@gmail.com>
2025-01-27 19:12:24 +0530

    date

--- date/date1.js
+++ date/date1.js
@@ -18,3 +18,27 @@ console.log("month..",date.getMonth())
 console.log("year..",date.getFullYear())
 console.log(date.getTime())
 
+
+
+var today = new Date()
+console.log(today)
+console.log(today.toString())
+console.log(today.toLocaleString())
+console.log("ld...",today.toLocaleDateString())
+console.log("ld...",today.toLocaleTimeString())
+console.log(today.toISOString()) //utc
+console.log(today.toTimeString())
+
+//utc time + 5:30 hrs --> 13:35 + 5.30 : 19:05
+
+
+
+var startDate = new Date("2025-01-01")
+console.log(startDate)
+var endDate = new Date("2025-01-27")
+console.log(endDate)
+
+const difftime = endDate-startDate;
+console.log(difftime)
+const diffDayes = difftime / (1000*60*60*24)
+console.log(diffDayes) //26
