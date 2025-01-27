b127b5564faca2bc6a775802316ac21a2291485f
samirvithlani <samir.vithlani83955@gmail.com>
2025-01-27 18:57:26 +0530

    date

---
+++ date/date1.js
@@ -0,0 +1,20 @@
+//js date class
+//class object
+
+var date = new Date()
+
+// date.setFullYear(2025)
+// date.setMonth(1)
+// date.setDate(14)
+// date.setMinutes(0)
+// date.setHours(0)
+// date.setSeconds(0)
+
+console.log(date)
+//1970 1st jan 00:00 1
+
+console.log("date...",date.getDate())
+console.log("month..",date.getMonth())
+console.log("year..",date.getFullYear())
+console.log(date.getTime())
+
